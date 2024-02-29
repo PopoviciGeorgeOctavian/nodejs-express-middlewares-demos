09a98976ed83235e922322dec9723ac6bc124e01
POPOVICI C.T. GEORGE-OCTAVIAN <popovicigeorge3@gmail.com>
2024-02-29 19:19:52 +0200

    Create index5.js

---
+++ index5.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var userValidator = false;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+function passwordGet(req, res, next)
+{
+  console.log(req.body);
+  const passwordd= req.body["password"];
+  if(passwordd === "LOGGING")
+  {
+    userValidator = true;
+  }
+  next();
+}
+
+app.use(passwordGet);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.post("/check", (req, res) => {
+    if(userValidator)
+    {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else
+    {
+        res.sendFile(__dirname + "/public/index.html");
+
+    }
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
